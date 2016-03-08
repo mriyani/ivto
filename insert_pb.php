cda031074ede41bdbe6eee58502374c46760f512
aCHeeQ <ryaneq@gmail.com>
2016-03-08 14:06:42 +0800

    db,insert_pb,style uppdated

--- insert_pb.php
+++ insert_pb.php
@@ -14,27 +14,23 @@
 			$add = "INSERT INTO pb (kodpb, namapb, alamatpb, notel, email, negeri, poskod)
 			VALUES ('$kodpb','$namapb', '$alamatpb', '$notel', '$email', '$negeri', '$poskod')";
 
-			if (mysqli_query($dbc,$add)) {
-				
-				
-			} {
+			if (!mysqli_query($dbc,$add)) {
 				echo "Error: " . mysql_errno() . mysqli_error($dbc);
-
 			}
 
-			$sel = "SELECT 'kodpb' FROM pb WHERE kodpb='$kodpb' LIMIT 1";
+			$sel = "SELECT 'id' FROM pb WHERE kodpb='$kodpb' LIMIT 1";
 			$result = mysqli_query($dbc,$sel);
 
 			if (mysqli_num_rows($result) > 0) {
+				//output data of each row
+				while($row = mysqli_fetch_assoc($result)) {
 
+				}
 			}
 
-
-
-			} {
 			// mysqli_close($dbc);
 			// header('Location: view.php');
-		}
+	} 
 ?>
 <!DOCTYPE html>
 <html>
@@ -52,49 +48,49 @@
 				<div class="form-group">
 					<span class="label label-success">Kod PB</span>
 					<div class="col-lg-6">
-						<div class="view"><?php echo "kodpb"; ?></div>
+						<div class="view"><?php echo $row['id']; ?></div>
 					</div>
 				</div>
 				<div class="form-group">
 					<span class="label label-success">Jenis PB</span>
 					<div class="col-lg-6">
-						<div class="view"><?php echo "$jenispb"; ?></div>
+						<div class="view"><?php echo $row['jenispb']; ?></div>
 					</div>
 				</div>
 				<div class="form-group">
 					<span class="label label-success">Nama PB</span>
 					<div class="col-lg-6">
-						<div class="view"><?php echo "$namapb"; ?></div>
+						<div class="view"><?php echo $row['namapb']; ?></div>
 					</div>
 				</div>
 				<div class="form-group">
 					<span class="label label-success">No. Tel</span>
 					<div class="col-lg-6">
-						<div class="view"><?php echo "$notel"; ?></div>
+						<div class="view"><?php echo $row['notel']; ?></div>
 					</div>
 				</div>
 				<div class="form-group">
 					<span class="label label-success">Email</span>
 					<div class="col-lg-6">
-						<div class="view"><?php echo "$email"; ?></div>
+						<div class="view"><?php echo $row['email']; ?></div>
 					</div>
 				</div>
 				<div class="form-group">
 					<span class="label label-success">Negeri</span>
 					<div class="col-lg-6">
-						<div class="view"><?php echo "$negeri"; ?></div>
+						<div class="view"><?php echo $row['negeri'] ?></div>
 					</div>
 				</div>
 				<div class="form-group">
 					<span class="label label-success">Alamat</span>
 					<div class="col-lg-6">
-						<div class="view"><?php echo "$alamatpb"; ?></div>
+						<div class="view"><?php echo $row['alamatpb']; ?></div>
 					</div>
 				</div>
 				<div class="form-group">
 					<span class="label label-success">Poskod</span>
 					<div class="col-lg-6">
-						<div class="view"><?php echo "$poskod"; ?></div>
+						<div class="view"><?php echo $row['poskod']; ?></div>
 					</div>
 				</div>
 			</fieldset><br>
